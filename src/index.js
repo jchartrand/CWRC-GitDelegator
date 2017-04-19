d5f224e6c635e653cdad0518f224e98ddd00089f
James Chartrand <jc.chartrand@gmail.com>
2017-04-19 12:42:42 +1000

    fix(document): parses default xml doc before loading

--- src/index.js
+++ src/index.js
@@ -9,33 +9,7 @@ var cwrcGit = require('cwrc-git-server-client');
 
 var cwrcAppName = "CWRC-GitWriter" + "-web-app";
 
-var blankTEIDoc = `
-    <?xml version="1.0" encoding="UTF-8"?>
-<?xml-model href="http://cwrc.ca/schemas/cwrc_tei_lite.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>
-<?xml-stylesheet type="text/css" href="http://cwrc.ca/templates/css/tei.css"?>
-<TEI xmlns="http://www.tei-c.org/ns/1.0" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cw="http://cwrc.ca/ns/cw#" xmlns:w="http://cwrctc.artsrn.ualberta.ca/#">
-    <teiHeader>
-        <fileDesc>
-            <titleStmt>
-                <title>Sample Document Title</title>
-            </titleStmt>
-            <publicationStmt>
-                <p></p>
-            </publicationStmt>
-            <sourceDesc sameAs="http://www.cwrc.ca">
-                <p>Created from original research by members of CWRC/CSÉC unless otherwise noted.</p>
-            </sourceDesc>
-        </fileDesc>
-    </teiHeader>
-    <text>
-        <body>
-            <div>
-                Replace with your text.
-            </div>
-        </body>
-    </text>
-</TEI>`;
-
+var blankTEIDoc = '<?xml version="1.0" encoding="UTF-8"?> <?xml-model href="http://cwrc.ca/schemas/cwrc_tei_lite.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?> <?xml-stylesheet type="text/css" href="http://cwrc.ca/templates/css/tei.css"?> <TEI xmlns="http://www.tei-c.org/ns/1.0" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cw="http://cwrc.ca/ns/cw#" xmlns:w="http://cwrctc.artsrn.ualberta.ca/#"> <teiHeader> <fileDesc> <titleStmt> <title>Sample Document Title</title> </titleStmt> <publicationStmt> <p></p> </publicationStmt> <sourceDesc sameAs="http://www.cwrc.ca"> <p>Created from original research by members of CWRC/CSÉC unless otherwise noted.</p> </sourceDesc> </fileDesc> </teiHeader> <text> <body> <div> Replace with your text. </div> </body> </text> </TEI>';
 /**
  * @class Delegator
  * @param {Writer} writer
@@ -774,7 +748,9 @@ function Delegator(writer) {
                 var contents = writer.editor.getContent({format: 'raw'});
                 console.log(contents);
               if (!contents.includes('_tag')) {
-                writer.loadDocument(blankTEIDoc)
+                var defaultxmlDoc = $.parseXML(blankTEIDoc);
+                w.fileManager.loadDocumentFromXml(defaultxmlDoc);
+                //writer.loadDocument(blankTEIDoc)
               }
             })
         } else {
